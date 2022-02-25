cb2edda8caedbca923d9e18672f0385a6cd5a4a8
074Matheus <matheuslive168@gmail.com>
2022-02-25 20:05:16 -0300

    Desafio 2

--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,7 @@ app.listen(PORT, () => {
 	let peso1 = 60
 	let altura1 = 1.70
 	let imcAluno1 = peso1 / (altura1 * altura1)
-	let dev1 = false
+	const dev1 = false
 
 
 	let aluno02 = 'Caio sousa'
@@ -53,7 +53,7 @@ app.listen(PORT, () => {
 	let peso2 = 75
 	let altura2 = 1.90
 	let imcAluno2 = peso2 / (altura2 * altura2)
-	let dev2 = false
+	const dev2 = false
 
 
 	let aluno03 = 'João silva'
@@ -61,14 +61,14 @@ app.listen(PORT, () => {
 	let peso3 = 80
 	let altura3 = 1.85
 	let imcAluno3 = peso3 / (altura3 * altura3)
-	let dev3 = true
+	const dev3 = true
 
 	var soma = nota1 + nota2 + nota3
 
 	console.log('A soma das idades é ', soma)
 	const alunos = [aluno01, aluno02, aluno03]
 
-	console.log('O nome dos alunos é')
+	console.log('O aluno0dos alunos é')
 
 	console.table(alunos)
 
@@ -77,15 +77,28 @@ app.listen(PORT, () => {
 	console.log('O imc do aluno2 é: ', imcAluno2)
 	console.log('O imc do aluno3 é: ', imcAluno3)
 
-	var media =  imcAluno1 + imcAluno2 + imcAluno3
+	var media = imcAluno1 + imcAluno2 + imcAluno3
 
-	console.log('A media do imc dos ubuntus é:',media)
-
-	
-
-	console.log('O aluno dev é: ', aluno03, dev3)
+	console.log('A media do imc dos ubuntus é:', media)
 
 	console.log('O aluno que possui sobrenome Silva é: ', aluno03)
+
+
+	if (dev1) {
+		console.log(`${aluno01} é dev`)
+	}else{
+		console.log(`${aluno01} não é dev`)
+	}
+	if (dev2) {
+		console.log(`${aluno02} é dev`)
+	} else {
+		console.log(`${aluno02} não é dev`)
+	}
+	if (dev3) {
+		console.log(`${aluno03} é dev`)
+	} else {
+		console.log(`${aluno03} não é dev`)
+	}
 
 	
 });
